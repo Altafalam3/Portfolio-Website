ec518626d19571c72b34d838a7dd3d5c0b8fbcc1
Altafalam3 <altaf.alam0032@gmail.com>
2023-01-21 17:04:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testmonials/Testimonials'
+import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
