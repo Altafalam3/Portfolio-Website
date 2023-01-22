b2c325f29042c661798481a8755dea3984a3e9c6
Altafalam3 <altaf.alam0032@gmail.com>
2023-01-23 00:09:29 +0530

    commit

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './testimonials.css'
+import AVT1 from '../../assets/avatar1.png'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='article'>
+          <div className="client__avatar">
+            <div className="client__avatar">
+              <img src={AVT1} alt="" />
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
